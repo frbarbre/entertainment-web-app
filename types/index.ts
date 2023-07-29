e07f94b872be50f8d5ef1b8e88abbbf43980319b
frbarbre <fr.barbre@gmail.com>
2023-07-29 10:57:37 +0200

    changed type

--- types/index.ts
+++ types/index.ts
@@ -1,19 +1,19 @@
 export type Media = {
-  title?: string;
-  thumbnail?: {
+  title: string;
+  thumbnail: {
     trending?: {
-      small?: string;
-      large?: string;
+      small: string;
+      large: string;
     };
-    regular?: {
-      small?: string;
-      medium?: string;
-      large?: string;
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
     };
   };
-  year?: number;
-  category?: string;
-  rating?: string;
-  isBookmarked?: boolean;
-  isTrending?: boolean;
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
 };
